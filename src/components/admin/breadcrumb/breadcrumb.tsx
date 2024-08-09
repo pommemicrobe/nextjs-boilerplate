6eeae8a4162ec2edbf88bcc14a2fda96d57743d9
Vincent <vincent@Vincents-MBP.lan>
2024-08-10 01:13:59 +0200

    Fix some warning in the console

--- src/components/admin/breadcrumb/breadcrumb.tsx
+++ src/components/admin/breadcrumb/breadcrumb.tsx
@@ -42,9 +42,9 @@ export default function Breadcrumb({ breadcrumb }: BreadcrumbProps) {
                 >
                   <path
                     stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="m1 9 4-4-4-4"
                   />
                 </svg>
